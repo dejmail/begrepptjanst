d9b3d7623a5e9e126e4a6aa4ff9a4b46c7d51be5
Liam Thompson <liam.thompson@vgregion.se>
2020-01-31 09:43:23 +0100

    change the swedish letters again to ASCII

--- ordbok/admin.py
+++ ordbok/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from ordbok.models import Begrepp, Beställare, Domän
+from ordbok.models import Begrepp, Bestallare, Doman
 
 admin.site.register(Begrepp)
 admin.site.register(Beställare)
