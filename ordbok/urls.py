30283a2d7a4943ac6c63fee9335d58c27285a56b
Liam Thompson <liam.thompson@vgregion.se>
2020-02-04 10:56:32 +0100

    updated url files

--- ordbok/urls.py
+++ ordbok/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import include, path
-
 from . import views
+from begrepptjanst.routers import router
 
 urlpatterns = [
     path('', views.index, name='index'),
+    #path('api/', include(router.urls))
 ]
