5a00d569cf3859dd028f716b00eed40bfcff290e
Olle Karlsson <SE2321000131-P000000314753@vgregion.se>
2020-02-04 13:48:50 +0100

    added some new versions of HTML (hello internet)

--- ordbok/views.py
+++ ordbok/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'view2.html')
+    return render(request, 'index.html')
+
+def view2(request):
+    return render(request,'view2.html')
+
+def view3(request):
+    return render(request, 'view3.html')
 
