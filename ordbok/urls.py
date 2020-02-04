0ac6d442ee2a3e7bff7dc094d52138c7ff2e5170
Liam Thompson <liam.thompson@vgregion.se>
2020-02-04 11:01:41 +0100

    did not save the removal of a dependency

--- ordbok/urls.py
+++ ordbok/urls.py
@@ -1,9 +1,7 @@
 
 from django.urls import include, path
 from . import views
-from begrepptjanst.routers import router
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #path('api/', include(router.urls))
 ]
