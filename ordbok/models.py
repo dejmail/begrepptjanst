c9b6226c4259671177c336bccd41e1677bda106c
Liam Thompson <liam.thompson@vgregion.se>
2020-02-06 15:56:29 +0100

    Changed some of the fields of the Begrepp table. Added the following, externt_id and register to capture any external locations that may map to the begrepp we have just added in. We have also added in vgr_id as we will have to create one for each. The begrepp-term confusion was cleared up, as the begrepp conists of the term and context.

--- ordbok/models.py
+++ ordbok/models.py
@@ -1,29 +1,59 @@
-from django.db import models
 from django.contrib import admin
+from django.db import models
 
 # Create your models here.
  
 class Begrepp(models.Model):
-    begrepp_key = models.IntegerField(primary_key=True)
-    beställare_id = models.ForeignKey("Bestallare", on_delete=models.CASCADE)
-    definition = models.TextField()
-    kontext_beskrivning = models.TextField()
-    synonym = models.CharField(max_length=255)
-    begrepp = models.CharField(max_length=255)
-    utländsk_definition = models.TextField()
-    utländsk_term = models.CharField(max_length=255)
-    begrepp_version_nummer = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "Begrepp"
+
+    DEFAULT_STATUS = 'Ny'
+    STATUS_VAL = (
+        'Avråds' : 'Avråds'
+        'Definiera ej' : 'Definiera ej', 
+        'Inte definierad' : 'Inte definierad', 
+        'Klar' : 'Klar', 
+        DEFAULT_STATUS : DEFAULT_STATUS, 
+        'Pågår' : 'Pågår', 
+        'Publicera ej' : 'Publicera ej'        
+    )
+
+    begrepp_kontext = models.TextField()
+    begrepp_version_nummer = models.DateField()
+    beställare_id = models.ForeignKey("Bestallare", on_delete=models.CASCADE)
+    definition = models.TextField()
+    externt_id = models.CharField(max_length=255)
+    externt_register = models.CharField(max_length=255)
+    status = models.CharField(max_length=255,choices=STATUS_VAL,default=DEFAULT_STATUS)
+    synonym = models.ManCharField(max_length=255)
+    term = models.CharField(max_length=255)
+    utländsk_definition = models.TextField()
+    utländsk_term = models.CharField(max_length=255)
+    vgr_id = models.CharField(max_length=255)
 
 class Bestallare(models.Model):
+    class Meta:
+        verbose_name_plural = "Beställare"
+
+    beställare_namn = models.CharField(max_length=255)
     beställare_datum = models.DateField()
     beställare_email = models.EmailField()
-    beställare_kontext = models.CharField(max_length=255)
-    beställare_namn = models.CharField(max_length=255)
     beställare_telefon = models.IntegerField()
     domän = models.ForeignKey("Doman", to_field='domän_id', on_delete=models.PROTECT, blank=True, null=True)
 
 class Doman(models.Model):
+        class Meta:
+        verbose_name_plural = "Domäner"
+
     domän_id = models.CharField(max_length=255, primary_key=True)
     domän_namn = models.CharField(max_length=255)
     domän_email = models.CharField(max_length=255)
 
+class Synonym(models.Model):
+
+        class Meta:
+        verbose_name_plural = "Synonymer"
+
+    synonym_id = models.IntegerField(primary_key=True)
+    synonym = models.CharField(max_length=255)
