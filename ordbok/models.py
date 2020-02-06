40c180844463cd5c391362009ab3a79095306c26
Liam Thompson <liam.thompson@vgregion.se>
2020-02-06 22:35:15 +0100

    fixed the syntax and removed some hidden characters from the text that were causing problems

--- ordbok/models.py
+++ ordbok/models.py
@@ -1,36 +1,33 @@
 from django.contrib import admin
 from django.db import models
 
-# Create your models here.
- 
 class Begrepp(models.Model):
 
     class Meta:
         verbose_name_plural = "Begrepp"
 
     DEFAULT_STATUS = 'Ny'
-    STATUS_VAL = (
-        'Avråds' : 'Avråds'
-        'Definiera ej' : 'Definiera ej', 
-        'Inte definierad' : 'Inte definierad', 
-        'Klar' : 'Klar', 
-        DEFAULT_STATUS : DEFAULT_STATUS, 
-        'Pågår' : 'Pågår', 
-        'Publicera ej' : 'Publicera ej'        
-    )
 
-    begrepp_kontext = models.TextField()
-    begrepp_version_nummer = models.DateField()
-    beställare_id = models.ForeignKey("Bestallare", on_delete=models.CASCADE)
-    definition = models.TextField()
-    externt_id = models.CharField(max_length=255)
-    externt_register = models.CharField(max_length=255)
-    status = models.CharField(max_length=255,choices=STATUS_VAL,default=DEFAULT_STATUS)
-    synonym = models.ManCharField(max_length=255)
-    term = models.CharField(max_length=255)
-    utländsk_definition = models.TextField()
-    utländsk_term = models.CharField(max_length=255)
-    vgr_id = models.CharField(max_length=255)
+    STATUS_VAL = (('Avråds', "Avråds"),
+                  ('Definiera ej', 'Definiera ej'), 
+                  ('Inte definierad', 'Inte definierad'), 
+                  ('Klar', 'Klar'), 
+                  ('Pågår', 'Pågår'), 
+                  ('Publicera ej', 'Publicera ej'),
+                  (DEFAULT_STATUS, DEFAULT_STATUS))
+
+    begrepp_kontext = models.TextField()
+    begrepp_version_nummer = models.DateField()
+    beställare_id = models.ForeignKey('Bestallare', to_field='id', on_delete=models.CASCADE)
+    definition = models.TextField()
+    externt_id = models.CharField(max_length=255, null=True)
+    externt_register = models.CharField(max_length=255, null=True)
+    status = models.CharField(max_length=255, choices=STATUS_VAL, default=DEFAULT_STATUS)
+    #synonym = models.ForeignKey('Synonym', on_delete=models.CASCADE)
+    term = models.CharField(max_length=255)
+    utländsk_definition = models.TextField()
+    utländsk_term = models.CharField(max_length=255)
+    vgr_id = models.CharField(max_length=255, null=True)
 
 class Bestallare(models.Model):
     class Meta:
@@ -43,7 +40,7 @@ class Bestallare(models.Model):
     domän = models.ForeignKey("Doman", to_field='domän_id', on_delete=models.PROTECT, blank=True, null=True)
 
 class Doman(models.Model):
-        class Meta:
+    class Meta:
         verbose_name_plural = "Domäner"
 
     domän_id = models.CharField(max_length=255, primary_key=True)
@@ -52,8 +49,8 @@ class Doman(models.Model):
 
 class Synonym(models.Model):
 
-        class Meta:
+    class Meta:
         verbose_name_plural = "Synonymer"
 
-    synonym_id = models.IntegerField(primary_key=True)
+    synonym_id = models.ForeignKey("Begrepp", to_field="id", on_delete=models.PROTECT, blank=True, null=True)
     synonym = models.CharField(max_length=255)
