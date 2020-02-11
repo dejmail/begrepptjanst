206003f7491db86afcfc0b95dd9a4acc7063dc0f
Liam Thompson <liam.thompson@vgregion.se>
2020-02-11 09:51:27 +0100

    added a simple ajax livesearch example

--- ordbok/views.py
+++ ordbok/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Begrepp
 
 def index(request):
     return render(request, 'index.html')
@@ -11,3 +11,16 @@ def view2(request):
 def view3(request):
     return render(request, 'view3.html')
 
+def autocompleteModel(request):
+    if request.is_ajax():
+        q = request.GET.get('term', '').capitalize()
+        search_qs = Begrepp.objects.filter(name__startswith=q)
+        results = []
+        print(q)
+        for r in search_qs:
+            results.append(r.FIELD)
+        data = json.dumps(results)
+    else:
+        data = 'fail'
+    mimetype = 'application/json'
+    return HttpResponse(data, mimetype)
