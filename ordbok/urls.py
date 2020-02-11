61fdf719ab01237005413e17530cffd10fb979b0
Liam Thompson <liam.thompson@vgregion.se>
2020-02-11 15:22:49 +0100

    working alterations that allow us to search through the database

--- ordbok/urls.py
+++ ordbok/urls.py
@@ -2,10 +2,14 @@
 from django.urls import include, path
 from . import views
 from .views import autocompleteModel
+from ordbok import views as ordbok_views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('view2', views.view2, name= "view2"),
     path('view3', views.view3 , name="view3"),
-    path('search', autocompleteModel, name='livesearch'),
+    path('base', views.base, name="base"),
+    path("begrepp/", ordbok_views.begrepp_view, name="begrepp"),
+
 ]
+
