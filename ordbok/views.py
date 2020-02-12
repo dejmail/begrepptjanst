d3415a8ec18a9592754b5ed3d635e65b3ab554d7
Liam Thompson <liam.thompson@vgregion.se>
2020-02-12 15:41:02 +0100

    a working display for the definition

--- ordbok/views.py
+++ ordbok/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from ordbok.models import Begrepp
 from django.template.loader import render_to_string
+from django.core import serializers
 import json
 from django.http import JsonResponse
+from pdb import set_trace
+from django.forms.models import model_to_dict
+from django.core.serializers.json import DjangoJSONEncoder
+
+
 
 def index(request):
     return render(request, 'index.html')
@@ -49,5 +55,32 @@ def begrepp_view(request):
         )
         data_dict = {"html_from_view": html}
         return JsonResponse(data=data_dict, safe=False)
+
+    return render(request, "term.html", context=ctx)
+
+def begrepp_förklaring_view(request):
+    ctx = {}
+    url_parameter = request.GET.get("q")
+
+    if url_parameter:
+        exact_term = Begrepp.objects.get(id__exact=url_parameter)
+        model_dict = model_to_dict(exact_term)
+        #term_json =serializers.serialize('json', [exact_term,])
+        #print(term_json)
+    else:
+        term_json = 'Error - Record not found'
+
+    ctx["begrepp"] = [exact_term,]
+    if request.is_ajax():
+        print('ajax request received')
+        # html = render_to_string(
+        #     template_name="term_forklaring.html", context={"begrepp" : term_json}
+        # )
+        # data_dict = {"html_from_view": html}
+        
+        #print(data_dict.get('html_from_view'))
+        
+        return HttpResponse(json.dumps(model_dict, cls=DjangoJSONEncoder), content_type="application/json; charset=ISO-8859-1")
+        #return render(request, "term_forklaring.html", context={'data' : term_json}, content_type="application/json")
 
     return render(request, "term.html", context=ctx)
