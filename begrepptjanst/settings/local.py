57a7ad5b57ca33d545c2a2985e67755602610c4c
Olle <mail@ollekarlsson.se>
2020-02-18 14:31:23 +0100

    make up for da pig

--- begrepptjanst/settings/local.py
+++ begrepptjanst/settings/local.py
@@ -6,26 +6,26 @@ import ordbok
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME' : 'H:\Mina dokument\coding\db.sqlite3'
-
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'vgrinfor_begrepp',
-        'USER': 'vgrinfor_admin',
-        'PASSWORD': 'YqvyYGm5cJMLmzt',
-        'HOST': 'vgrinformatik.se',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
-        'DEFAULT-CHARACTER-SET' : 'utf8',
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME' : 'ordlist_databas.sqlite3'
+
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', 
+#         'NAME': 'vgrinfor_begrepp',
+#         'USER': 'vgrinfor_admin',
+#         'PASSWORD': 'YqvyYGm5cJMLmzt',
+#         'HOST': 'vgrinformatik.se',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#         'DEFAULT-CHARACTER-SET' : 'utf8',
+        
+#     }
+# }
 
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 TEMPLATE_DIRS = [PROJECT_PATH + '/templates/',]
