1f06ad0cb4fb78a0fa45d6901d6e65255482db42
Olle <mail@ollekarlsson.se>
2020-04-03 13:01:23 +0200

    Kontextlänk & ny status: preliminär

--- ordbok/models.py
+++ ordbok/models.py
@@ -10,6 +10,7 @@ STATUS_VAL = (('Avråds', "Avråds"),
               ('Klar', 'Klar'), 
               ('Pågår', 'Pågår'), 
               ('Publicera ej', 'Publicera ej'),
+              ('Preliminär', 'Preliminär')
               (DEFAULT_STATUS, DEFAULT_STATUS))
 
             
