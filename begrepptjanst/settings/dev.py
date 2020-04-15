0b124831a6659f2295eaa1682e8f1f0dd8edaa8b
Liam Thompson <dejmail@gmail.com>
2020-04-15 10:16:13 +0200

    added a dev database and configuration file

---
+++ begrepptjanst/settings/dev.py
@@ -0,0 +1,30 @@
+from begrepptjanst.settings.base import *
+
+DEBUG=False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'vgrinfor_backup',
+        'USER': 'vgrinfor_admin',
+        'PASSWORD': 'YqvyYGm5cJMLmzt',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+        'DEFAULT-CHARACTER-SET' : 'utf8',
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = '/home/vgrinfor/public_html/begrepptjanst/static'
+STATICFILES_DIRS = ['/home/vgrinfor/begrepptjanst/static',]
+STATIC_URL = '/begrepptjanst/static/'
+
+# Email settings
+
+EMAIL_HOST = 'mail.vgrinformatik.se'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'info@vgrinformatik.se'
+EMAIL_HOST_PASSWORD = 'XrT5bsRq@[ks'
+EMAIL_USE_TLS = True
