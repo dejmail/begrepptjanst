993320f0429f5575620e72aee2526f2728ef5a72
Liam Jed Thompson <dejmail@gmail.com>
2020-08-31 10:09:06 +0200

    found a good screencast code example that allows us to take screencasts of all sorts including individual tabs in Chrome for example. I dont need to use iframes for some example, so I think it is a good addition. I think for normal screenshots we can simply allow an upload of a file.

--- ordbok/views.py
+++ ordbok/views.py
@@ -433,7 +433,8 @@ from django.views.decorators.clickjacking import xframe_options_exempt
 def take_a_screenshot(request):
     #set_trace()
     request.META['X-Frame-Options'] = 'ALLOW-FROM 127.0.0.1'
-    return render(request, 'upload_context_video_or_pic.html', {})
+    return render(request, 'screencast.html', {})
+    #return render(request, 'upload_context_video_or_pic.html', {})
     #return HttpResponse("This page is safe to load in a frame on any site.")
 
 @xframe_options_exempt
