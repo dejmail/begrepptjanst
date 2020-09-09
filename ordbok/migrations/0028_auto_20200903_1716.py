067d551775773b97909d5d456f0b78436f3853f1
Liam Jed Thompson <dejmail@gmail.com>
2020-09-09 08:47:52 +0200

    removed the adding and removal of a field in begrepp as this was just confusing the migration on the mysql server

--- ordbok/migrations/0028_auto_20200903_1716.py
+++ ordbok/migrations/0028_auto_20200903_1716.py
@@ -11,10 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='begrepp',
-            name='support_files',
-        ),
         migrations.CreateModel(
             name='BegreppExternalFiles',
             fields=[
