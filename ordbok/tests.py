faa820fb8815fb2f2929f00a2518d57191503d09
Liam Jed Thompson <dejmail@gmail.com>
2020-10-09 10:12:32 +0200

    first test examples

--- ordbok/tests.py
+++ ordbok/tests.py
@@ -1,3 +1,16 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from .views import begrepp_view
 
-# Create your tests here.
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, begrepp_view)
+    
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = begrepp_view(request)
+        self.assertTrue(response.content.startswith(b'<!doctype html>'))
+        self.assertIn('<title>VGR Informatik - OLLI Begreppstjänst</title>', response.content)
+        self.assertTrue(response.content.endswith('</html>'))
