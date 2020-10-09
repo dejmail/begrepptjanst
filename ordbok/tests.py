6811b660bec179a08e7e8703ab1c1403a931541e
Liam Jed Thompson <dejmail@gmail.com>
2020-10-09 09:51:52 +0200

    first tests

--- ordbok/tests.py
+++ ordbok/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse, resolve
+from django.test import TestCase
+from django.http import HttpRequest
+
+from .views import begrepp_view
+
+class OLLIPageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        # find what function this url maps to
+        found = resolve('/') #
+        self.assertEqual(found.func, begrepp_view)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn('<title>VGR Informatik - OLLI Begreppstjänst</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
