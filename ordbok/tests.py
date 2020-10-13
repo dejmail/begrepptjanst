aac649c5c445408a6242f628b47974b7c5270f63
Liam Jed Thompson <dejmail@gmail.com>
2020-10-13 08:52:22 +0200

    added the first tests for the app

--- ordbok/tests.py
+++ ordbok/tests.py
@@ -11,6 +11,10 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = begrepp_view(request)
-        self.assertTrue(response.content.startswith(b'<!doctype html>'))
-        self.assertIn('<title>VGR Informatik - OLLI Begreppstjänst</title>', response.content)
-        self.assertTrue(response.content.endswith('</html>'))
+        self.assertTrue(response.content.startswith(b'\n'))
+        self.assertIn(b'<title style="font-family: frutiger;">VGR Informatik - OLLI Begreppstj\xc3\xa4nst</title>', response.content)
+        self.assertTrue(response.content.endswith(b'\n'))
+        self.assertIn(b'function whatDoYouWant(requested_term)', msg='cannot find this function in the HTML', container=response.content)
+        self.assertIn(b'function populate_request_form(requested_term)', msg='cannot find this function in the HTML', container=response.content)
+        self.assertIn(b'function scrollToHelp()', msg='cannot find this function in the HTML', container=response.content)
+        self.assertIn(b'function clear_mittenspanrow()', msg='cannot find this function in the HTML', container=response.content)
