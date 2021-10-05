564bdce259d17d734e08425411ecdd32294b4d8f
Liam Jed Thompson <dejmail@gmail.com>
2021-10-05 11:28:38 +0200

    added the template tags that I needed

---
+++ ordbok/templatetags/get_item.py
@@ -0,0 +1,10 @@
+from django.template.defaulttags import register
+from django import template
+from pdb import set_trace
+
+register = template.Library()
+
+@register.filter(name='get_item')
+def get_item(dictionary, key):
+    
+    return dictionary.get(key)
