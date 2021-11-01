39a3e385454daaf2337aca340137e8e16f3cd732
Liam Jed Thompson <dejmail@gmail.com>
2021-11-01 09:08:01 +0100

    had to change the collation on the server so that the DB searches could tell the difference between Å Ä Ö and A O. Had to remove the charset setting for it to work though.

--- begrepptjanst/settings/dev.py
+++ begrepptjanst/settings/dev.py
@@ -15,7 +15,6 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             # Tell MySQLdb to connect with 'utf8mb4' character set
-            'charset': 'utf8mb4',
             'sql_mode' : 'traditional',
         },
          'TEST': {
--- begrepptjanst/settings/production.py
+++ begrepptjanst/settings/production.py
@@ -12,7 +12,7 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             # Tell MySQLdb to connect with 'utf8mb4' character set
-            'charset': 'utf8mb4',
+            # 'charset': 'utf8mb4',
             'sql_mode' : 'traditional',
         },
          'TEST': {
