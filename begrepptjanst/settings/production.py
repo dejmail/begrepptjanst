4f511aefe94ba75daa4c2c4ef9ed32186619a1d8
Liam Jed Thompson <dejmail@gmail.com>
2021-11-02 09:33:17 +0100

    removed the encoding for the db as it is specified on the DB

--- begrepptjanst/settings/production.py
+++ begrepptjanst/settings/production.py
@@ -11,8 +11,6 @@ DATABASES = {
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
         'OPTIONS': {
-            # Tell MySQLdb to connect with 'utf8mb4' character set
-            # 'charset': 'utf8mb4',
             'sql_mode' : 'traditional',
         },
          'TEST': {
