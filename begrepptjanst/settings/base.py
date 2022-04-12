e6c6cfc07072eeb2da0dd76dab7b0c04bdc42c95
Liam Jed Thompson <dejmail@gmail.com>
2022-04-12 16:54:16 +0200

    added the secret key to the env variables

--- begrepptjanst/settings/base.py
+++ begrepptjanst/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 ALLOWED_HOSTS = ['vgrinformatik.se', '127.0.0.1','localhost']
 
