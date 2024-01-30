a1df46b1c12f5a0ee1cb71ac6f520223ce43f2da
Liam <liam.thompson@vgregion.se>
2024-01-30 15:17:52 +0100

    Trying to add an onload so that this only fires at the end of the page load

--- static/javascript/image-preview.js
+++ static/javascript/image-preview.js
@@ -1,13 +1,15 @@
-django.jQuery(document).ready(function($) {
-    $('.image-thumbnail').click(function() {
-        var imageUrl = $(this).attr('src');
-        var modalHtml = '<div class="image-modal"><img src="' + imageUrl + '"></div>';
-        $(modalHtml).appendTo('body').fadeIn();
-    });
+window.onload = function() {
+    django.jQuery(document).ready(function($) {
+        $('.image-thumbnail').click(function() {
+            var imageUrl = $(this).attr('src');
+            var modalHtml = '<div class="image-modal"><img src="' + imageUrl + '"></div>';
+            $(modalHtml).appendTo('body').fadeIn();
+        });
 
-    $(document).on('click', '.image-modal', function() {
-        $(this).fadeOut(function() {
-            $(this).remove();
+        $(document).on('click', '.image-modal', function() {
+            $(this).fadeOut(function() {
+                $(this).remove();
+            });
         });
     });
-});
+};
