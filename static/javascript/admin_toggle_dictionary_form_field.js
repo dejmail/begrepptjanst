835cc9d3ef806ee6b3e8f297e40902c2f0887c2a
Liam <liam.thompson@vgregion.se>
2024-02-01 12:57:24 +0100

    Removed a debugger statement that was pausing things

--- static/javascript/admin_toggle_dictionary_form_field.js
+++ static/javascript/admin_toggle_dictionary_form_field.js
@@ -2,11 +2,10 @@ window.onload = function() {
     var currentUrl = window.location.href;
     var baseUrl = currentUrl.split('/').slice(0,4).join('/')+'/';
     var url = baseUrl + 'get_config_options/?option=dictionaries_inline';
-    console.log('Getting ' + url);
+    console.log('Getting config from ' + url);
     django.jQuery(document).ready(function() {
         django.jQuery.get(url, function(data) {
-            var configOptionValue = data.is_active;  // Adjust this based on your JSON response structure
-            debugger;
+            var configOptionValue = data.is_active;
             if (configOptionValue === false) {
                 django.jQuery('div.field-dictionaries').closest('.form-row').hide();
             } else {
