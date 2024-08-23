2fe4334f55b97cc72cef5a10c6bc73dd80c7aee3
Liam <liam.thompson@vgregion.se>
2024-08-23 14:09:27 +0200

    changed a function name back to english

--- ordbok/urls.py
+++ ordbok/urls.py
@@ -5,10 +5,10 @@ from ordbok import admin as admin_views
 
 
 urlpatterns = [
-    path("", views.begrepp_view, name="begrepp"),
+    path('', views.begrepp_view, name="begrepp"),
     path('kommentera/', views.kommentera_term, name="kommentera_term"),
     path('begrepp-forklaring/', views.begrepp_förklaring_view, name="begrepp_förklaring"),
-    path('requesttermform/', views.hantera_request_term, name='hantera_term_request'),
+    path('requesttermform/', views.request_new_term, name='hantera_term_request'),
     path('unread-comments/', views.return_number_of_comments, name='unread_comments'),
     path('prenumera/', views.prenumera_till_epost, name="email_subscribe"),
     path('no-search-result/', views.no_search_result, name='no_search_result'),
