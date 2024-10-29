ffc7b908dca88a186d9e7ca77035437d738af56a
Liam <liam.thompson@vgregion.se>
2024-10-29 12:42:06 +0100

    Fixed the url construction for the server

--- static/javascript/about-card-text.js
+++ static/javascript/about-card-text.js
@@ -1,10 +1,9 @@
-const baseUrl = home_url;
-
 document.getElementById('dictionary-select').addEventListener('change', function () {
     const selectedValue = this.value;
-    
+    console.log('Getting new about text for card');
+    console.log(`${baseUrl}get-dictionary-content/${selectedValue}`);
     // Make an AJAX call to the Django view
-    fetch(`${baseUrl}/get-dictionary-content/${selectedValue}`)
+    fetch(`${baseUrl}get-dictionary-content/${selectedValue}`)
       .then(response => response.json())
       .then(html => {
         document.getElementById('about-card-text').innerHTML = html.dictionary_context;
