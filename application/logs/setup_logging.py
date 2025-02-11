cc350c93c4eff04a192c1bd647a742471bb81a9f
Liam <liam.thompson@vgregion.se>
2025-02-11 15:15:55 +0100

    Changed the loggin so I can see which line number it is coming from

--- application/logs/setup_logging.py
+++ application/logs/setup_logging.py
@@ -21,7 +21,7 @@ logging_schema = {
             # class is always "logging.Formatter"
             "class": "logging.Formatter",
             # Optional: logging output format
-            "format": "%(asctime)s\t%(levelname)s\t%(filename)s\t%(message)s",
+            "format": "%(asctime)s\t%(levelname)s\t%(filename)s:%(lineno)d\t%(message)s",
             # Optional: asctime format
             "datefmt": "%d %b %y %H:%M:%S"
         }
@@ -29,42 +29,42 @@ logging_schema = {
     # Handlers use the formatter names declared above
     "handlers": {
         # Name of handler
-        # "console": {
-        #     # The class of logger. A mixture of logging.config.dictConfig() and
-        #     # logger class-specific keyword arguments (kwargs) are passed in here. 
-        #     "class": "logging.StreamHandler",
-        #     # This is the formatter name declared above
-        #     "formatter": "standard",
-        #     "level": "DEBUG",
-        #     # The default is stderr
-        #     "stream": "ext://sys.stdout"
-        # },
+        "console": {
+            # The class of logger. A mixture of logging.config.dictConfig() and
+            # logger class-specific keyword arguments (kwargs) are passed in here. 
+            "class": "logging.StreamHandler",
+            # This is the formatter name declared above
+            "formatter": "standard",
+            "level": "DEBUG",
+            # The default is stderr
+            "stream": "ext://sys.stdout"
+        },
         # Same as the StreamHandler example above, but with different
         # handler-specific kwargs.
-        "file": {  
-            "class": "logging.handlers.RotatingFileHandler",
-            "formatter": "standard",
-            "level": "ERROR",
-            "filename": "logs.log",
-            "mode": "a",
-            "encoding": "utf-8",
-            "maxBytes": 500000,
-            "backupCount": 4
-        }
+        # "file": {  
+        #     "class": "logging.handlers.RotatingFileHandler",
+        #     "formatter": "standard",
+        #     "level": "ERROR",
+        #     "filename": "logs.log",
+        #     "mode": "a",
+        #     "encoding": "utf-8",
+        #     "maxBytes": 500000,
+        #     "backupCount": 4
+        # }
     },
     # Loggers use the handler names declared above
     "loggers" : {
         "__main__": {  # if __name__ == "__main__"
             # Use a list even if one handler is used
-            "handlers": ["file"],
-            "level": "ERROR",
+            "handlers": ["console"],
+            "level": "DEBUG",
             "propagate": False
         }
     },
     # Just a standalone kwarg for the root logger
     "root" : {
-        "level": "ERROR",
-        "handlers": ["file"]
+        "level": "DEBUG",
+        "handlers": ["console"]
     }
 }
 
